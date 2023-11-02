8f9ae7b983efa3bc088422444ece49be74195f00
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-01 23:38:54 -0300

    v3

--- app/Detalle/[id]/[repartoCompleto]/page.jsx
+++ app/Detalle/[id]/[repartoCompleto]/page.jsx
@@ -1,17 +1,18 @@
 import HeaderReparto from "@/components/HeaderReparto/HeaderReparto"
 import '../[repartoCompleto]/repartoCompleto.css'
 import TarjetaActor from "@/components/TarjetaActor/TarjetaActor"
-import { RepartoPelicula } from "@/app/peticionesFetch/page"
+import { RepartoPelicula, buscarPelicula } from "@/app/peticionesFetch/page"
 
 async function repartoCompleto ( {params}){
   let reparto = await RepartoPelicula(params.id)
+  let pelicula = await buscarPelicula(params.id)
   let prueba = reparto.crew[1]
 
 
     console.log({prueba})
     return (
         <div className="repartoCompleto">
-            <HeaderReparto />
+            <HeaderReparto pelicula={pelicula} />
             <div className="repartoCompletoTarjetas">
                  <div className="contaier__repartoCompleto">
                  <h1>Reparto</h1>
--- components/HeaderReparto/HeaderReparto.jsx
+++ components/HeaderReparto/HeaderReparto.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import '../HeaderReparto/headerReparto.css'
-const HeaderReparto = ()=>{
+const HeaderReparto = ( {pelicula} )=>{
+    const urlImagen = `https://image.tmdb.org/t/p/original/${pelicula.poster_path}`
+
    return (
     <div className="headerReparto">
-        <img src="https://res.cloudinary.com/dcf9eqqgt/image/upload/v1698623623/App%20peliculas/scale_ytxylo.jpg"></img>
+        <img src={urlImagen}></img>
         <div className="headerReparto__info">
-        <h1 className="headerReparto__info__titulo">Titulo de la pelicula(2015)</h1>
+        <h1 className="headerReparto__info__titulo">{pelicula.title}</h1>
         <h1 className="headerReparto__info__volver">Volver</h1>
         </div>
     </div>
