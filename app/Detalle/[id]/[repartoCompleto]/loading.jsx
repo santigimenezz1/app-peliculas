15d12829b8970a2807dbe4cb2d9f308c12ea9aa1
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-07 23:32:39 -0300

    vercel

--- app/Detalle/[id]/[repartoCompleto]/loading.jsx
+++ app/Detalle/[id]/[repartoCompleto]/loading.jsx
@@ -1,5 +1,5 @@
  import { Skeleton } from '@mui/material'
-import '../../../detalle/[id]/[repartoCompleto]/loading.css'
+import '@/app/detalle/[id]/[repartoCompleto]/loading.css'
 const LoadingRepartoCompleto = ()=>{
     return (
         <div className="loadingRepartoCompleto">
