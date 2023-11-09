b6c600e397d8b14bc88b05a14f59cd2371d62ca1
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-09 19:43:17 -0300

    test

--- app/Detalle/[id]/[repartoCompleto]/page.jsx
+++ app/Detalle/[id]/[repartoCompleto]/page.jsx
@@ -6,53 +6,59 @@ import Link from "next/link";
 import LoadingRepartoCompleto from "./loading";
 
 async function repartoCompleto({ params }) {
+  let reparto, pelicula;
+
   try {
     const idPelicula = params.id;
-    const reparto = await RepartoPelicula(params.id);
-    const pelicula = await buscarPelicula("movie", params.id);
+    reparto = await RepartoPelicula(params.id);
+    pelicula = await buscarPelicula("movie", params.id);
 
     if (!reparto || !reparto.cast || !reparto.crew) {
-      // Manejar el caso en el que los datos de reparto no estén disponibles
-      return <LoadingRepartoCompleto />;
+      throw new Error("Los datos de reparto no están disponibles.");
     }
-
-    return (
-      <>
-        <div className="repartoCompleto">
-          <div>
-            <HeaderReparto idPelicula={idPelicula} pelicula={pelicula} />
-          </div>
-
-          <div className="repartoCompletoTarjetas">
-            <div className="contaier__repartoCompleto">
-              <h1>Reparto</h1>
-              <div className="repartoCompletoTarjetas__cast">
-                {reparto.cast.map((actor) => (
-                  <Link key={actor.id} style={{ textDecoration: "none" }} href={`/detalle/${params.id}/${params.id}/biografia/${actor.id}`}>
-                    <TarjetaActor key={actor.cast_id || actor.id} actor={actor} />
-                  </Link>
-                ))}
-              </div>
-            </div>
-            <div className="contaier__repartoCompleto">
-              <h1>Equipo</h1>
-              <div className="repartoCompletoTarjetas__cast">
-                {reparto.crew.map((actor) => (
-                  <Link key={actor.id} style={{ textDecoration: "none" }} href={`/detalle/${params.id}/${params.id}/biografia/${actor.id}`}>
-                    <TarjetaActor key={actor.crew_id || actor.id} actor={actor} />
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
   } catch (error) {
     // Manejar errores de la solicitud
     console.error("Error en la solicitud de datos:", error);
-    return <div>Error al cargar los datos.</div>;
+
+    return (
+      <>
+        <div>Error al cargar los datos.</div>
+      </>
+    );  
   }
+
+  return (
+    <>
+      <div className="repartoCompleto">
+        <div>
+          <HeaderReparto idPelicula={idPelicula} pelicula={pelicula} />
+        </div>
+
+        <div className="repartoCompletoTarjetas">
+          <div className="contaier__repartoCompleto">
+            <h1>Reparto</h1>
+            <div className="repartoCompletoTarjetas__cast">
+              {reparto.cast.map((actor) => (
+                <Link key={actor.id} style={{ textDecoration: "none" }} href={`/detalle/${params.id}/${params.id}/biografia/${actor.id}`}>
+                  <TarjetaActor key={actor.cast_id || actor.id} actor={actor} />
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="contaier__repartoCompleto">
+            <h1>Equipo</h1>
+            <div className="repartoCompletoTarjetas__cast">
+              {reparto.crew.map((actor) => (
+                <Link key={actor.id} style={{ textDecoration: "none" }} href={`/detalle/${params.id}/${params.id}/biografia/${actor.id}`}>
+                  <TarjetaActor key={actor.crew_id || actor.id} actor={actor} />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default repartoCompleto;
--- components/TarjetaActor/TarjetaActor.jsx
+++ components/TarjetaActor/TarjetaActor.jsx
@@ -3,15 +3,13 @@ import '../TarjetaActor/tarjetaActor.css';
 import '@/app/mediaQuery.css';
 
 const TarjetaActor = ({ actor }) => {
-  let urlImagen = "";
-  let name = "";
-  let castId = actor.cast_id || 0; // Usar 0 si cast_id es undefined o nulo
+  let urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png";
+  let name = "Nombre Desconocido";
+  let castId = actor && actor.cast_id ? actor.cast_id : 0; // Usar 0 si cast_id es undefined o nulo
 
   if (actor && actor.profile_path && actor.name) {
     urlImagen = `https://image.tmdb.org/t/p/original/${actor.profile_path}`;
     name = actor.name;
-  } else {
-    urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png";
   }
 
   return (
