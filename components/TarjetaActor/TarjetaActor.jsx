f1c61dc6584e5bea17971bfd584d0c17ea08df66
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-09 18:35:51 -0300

    test2

--- components/TarjetaActor/TarjetaActor.jsx
+++ components/TarjetaActor/TarjetaActor.jsx
@@ -7,8 +7,8 @@ const TarjetaActor = ( {actor} ) => {
 
  let urlImagen = "";
 
-    if (peticionActor.profile_path) {
-        urlImagen = `https://image.tmdb.org/t/p/original/${peticionActor.profile_path}`;
+    if(actor.profile_path) {
+        urlImagen = `https://image.tmdb.org/t/p/original/${actor.profile_path}`;
       }else{
         urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png"
       }
