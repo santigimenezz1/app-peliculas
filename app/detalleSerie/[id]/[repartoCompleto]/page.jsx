220b1caecaf13284b261eed94a7858258a741dcb
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-09 16:29:41 -0300

    test vercel

--- app/detalleSerie/[id]/[repartoCompleto]/page.jsx
+++ app/detalleSerie/[id]/[repartoCompleto]/page.jsx
@@ -1,5 +1,5 @@
 import HeaderReparto from "@/components/HeaderReparto/HeaderReparto";
-import '../../../detalle/[id]/[repartoCompleto]/repartoCompleto.css'
+import '../[repartoCompleto]/repartoCompleto.css';
 import TarjetaActor from "@/components/TarjetaActor/TarjetaActor";
 import {  RepartoSerie, buscarPelicula } from "@/app/peticionesFetch/page";
 import Link from "next/link";
