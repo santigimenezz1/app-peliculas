fdf59d3961b9a90ce94d4e6ae3d0b5d3e67d6b27
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-09 17:46:13 -0300

    test 2

--- app/Detalle/page.jsx
+++ app/Detalle/page.jsx
@@ -21,13 +21,11 @@ const Detalle = ( {idYoutube, data} ) => {
     setShowVideo(false);
   };
 
-  let urlImagen = ""; // Define urlImagen fuera del bloque condicional
-
-  if (data.poster_path) {
-    urlImagen = `https://image.tmdb.org/t/p/original/${data.poster_path}`;
-  }else{
-    urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png"
-  }
+  let urlImagen = "";
+if (data.poster_path) {
+  urlImagen = `https://image.tmdb.org/t/p/original/${data.poster_path}`;
+} else {
+  urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png";
 
   return (
     <>
--- components/TarjetaSerie/TarjetaSerie.jsx
+++ components/TarjetaSerie/TarjetaSerie.jsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 import '@/app/mediaQuery.css'
 
 const TarjetaSerie = ( {movie} ) => {
-  let urlImagen = ""
-  if (movie.poster_path) {
-      urlImagen = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
-    }else{
-      urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png"
-    }  return (
+  let urlImagen = "";
+if (movie.poster_path) {
+  urlImagen = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+} else {
+  urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png";
+}  return (
     <div className='tarjetaSerie'>
     <img className='tarjetaSerie__img' src={urlImagen}></img>
     </div>
