e04a7a42e02542aafb60a4b0e3b47932184acbd5
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-10 17:02:19 -0300

    error ruta solucionado

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/app/detalle/:id',
+          destination: '/app/detalle/[id]',
+        },
+      ];
+    },
+  };
+  
