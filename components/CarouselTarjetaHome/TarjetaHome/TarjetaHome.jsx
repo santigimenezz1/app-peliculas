dea01a2e6cd52daecc65b323016a8e69dce5a658
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-11-09 21:35:54 -0300

    correccion

--- components/CarouselTarjetaHome/TarjetaHome/TarjetaHome.jsx
+++ components/CarouselTarjetaHome/TarjetaHome/TarjetaHome.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 import '../TarjetaHome/tarjetaHome.css'
 
 const TarjetaHome = ( {movie} ) => {
-  let urlImagen = ""; 
-
-  if (movie.poster_path) {
-    urlImagen = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
-  }else{
-    urlImagen = "https://res.cloudinary.com/dcf9eqqgt/image/upload/v1699562010/App%20peliculas/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093_r2tucp.png"
-  }
+   const urlImagen = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
     return (
     <div className='tarjetaHome'>
       <h1>{movie.title}</h1>
